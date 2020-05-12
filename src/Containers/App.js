746a23f40b360dc84ca0f633623bfc77702eb6c3
Dannus90 <61561009+Dannus90@users.noreply.github.com>
2020-05-12 23:36:59 +0200

    internal proxy

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
 
     componentDidMount() {
         const url = "https://jsonplaceholder.typicode.com/users";
-        const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+        const proxyUrl = "https://git.heroku.com/stormy-fortress-06395.git";
         fetch(proxyUrl + url)
             .then((response) => response.json())
             .then((users) => this.setState({ robots: users }));
